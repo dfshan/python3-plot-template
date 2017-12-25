c382d8c0c3f7e4c66d2349cc4264b830bc62f71e
Danfeng Shan <shandanf@gmail.com>
2017-12-25 12:15:37 +0800

    [Update] update readme

--- Readme.md
+++ Readme.md
@@ -10,6 +10,17 @@ This is a template about reading data with `pandas` and plotting data with `matp
 ## Install requirements in Centos 7
 
 ```shell
-# yum install python34 python34-tkinter
-# pip3 install -r requirements.txt
+yum install python34 python34-tkinter
+pip3 install -r requirements.txt
 ```
+
+# Use customized paper style
+1. Copy the style file `paper.mplstyle` into `mpl_configdir/stylelib`,
+where `mpl_configdir` denotes the configdir of matplotlib.
+`mpl_configdir` can be get by `matplotlib.get_configdir()`.
+```shell
+# In MacOS
+mkdir -p ~/.matplotlib/stylelib/
+cp paper.mplstyle ~/.matplotlib/stylelib/
+```
+2. Uncomment the line `plt.style.use('paper')` in `plot.py`
--- plot.py
+++ plot.py
@@ -2,6 +2,45 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
+# line styles
+styles=[
+    {
+        'color': '#a00000',
+        'ls' : '-',
+        'marker' : 'o',
+        'markevery' : 1,
+        'markersize' : 12,
+        'markerfacecolor' : 'None',
+    },
+    {
+        'color': '#00a000',
+        'ls' : '-',
+        'marker' : 'x',
+        'markevery' : 1,
+        'markerfacecolor' : 'None',
+    },
+    {
+        'color': '#5060d0',
+        'ls' : '-',
+        'marker' : '>',
+        'markersize' : 12,
+        'markevery' : 1,
+    },
+    {
+        'color': '#f25900',
+        'ls' : '-',
+        'marker' : 'd',
+        'markevery' : 3,
+        'markerfacecolor' : 'None',
+    },
+    {
+        'color': '#500050',
+        'ls' : '*',
+        'marker' : 'd',
+        'markevery' : 1,
+        'markerfacecolor' : 'None',
+    }
+]
 
 def main():
     df = pd.read_csv(
@@ -10,7 +49,9 @@ def main():
         names=('x', 'y'),
         usecols=(0, 1),
     )
-    df.plot(x='x', y='y', grid=True)
+    #plt.style.use('paper')
+    df.plot(x='x', y='y', **styles[0])
+    plt.tight_layout()
     plt.show()
 
 
